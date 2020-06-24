a59ffeebedf30b32f3a9b8a4d6479195b2e54381
jorgemanzo <jmanzo203689@gmail.com>
2020-06-23 22:39:21 -0700

    Learned about the stack

--- 04/boot_sect_simple.asm
+++ 04/boot_sect_simple.asm
@@ -37,3 +37,71 @@
 ;             +-------------------------+ 0x400
 ;     1024<   | Interrupt Vector Table  |
 ;             +-------------------------+ 0x0
+
+; Set tty mode
+mov ah, 0x0e 
+
+; Set the stack's base pointer
+mov bp, 0x8000
+
+; Set the stack pointer to start at the base and grow DOWNWARD
+mov sp, bp
+
+; Push some things on the stack (note these are UNICODE)
+push 'A'
+push 'B'
+push 'C'
+
+; Heres what this looks like now....
+;             |                         | 
+;             |          Free           | 
+;             |                         | 
+; bp ----->   |-------------------------| <--- We're about to put a stack here, at 0x8000
+; Grows V     |           'A'           | <--- Lives at 0x7ffe (occupies 2 Byte UNICODE)
+; Down  V     |           'B'           | <--- Lives at 0x7ffc (occupies 2 Byte UNICODE)
+; sp ----->   |           'C'           | <--- Lives at 0x7ffa (occupies 2 Byte UNICODE)
+;             |~~~~~~~~~~~~~~~~~~~~~~~~~|
+;             |                         |
+;             |          Free           |
+;             +-------------------------+ 0x7e00
+;     512<    | Loaded boot sector (us) |
+;             +-------------------------+ 0x7c00
+;             |                         |
+
+; Remember!
+; "and off the stack:  in 16-bit mode, the stack works only on 16-bit boundaries."
+; 2-Bytes!
+
+; Print A
+mov al, [0x7ffe]
+int 0x10
+
+; Print B
+mov al, [0x7ffc]
+int 0x10
+
+; Print C
+mov al, [0x7ffa]
+int 0x10
+
+; Pop off the stack, C
+pop bx
+mov al, bl
+int 0x10
+
+; Pop off the stack, B
+pop bx
+mov al, bl
+int 0x10
+
+; Pop off the stack, A
+pop bx
+mov al, bl
+int 0x10
+
+; Palindrome! Now the stack is empty.
+
+; Magic
+jmp $
+times 510-($-$$) db 0
+dw 0xaa55
